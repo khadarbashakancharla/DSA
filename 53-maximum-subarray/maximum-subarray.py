cdcda033c91a697eb6265513bc9eedba030b5c2b
khadarbashakancharla <122791410+khadarbashakancharla@users.noreply.github.com>
2025-03-08 12:45:26 +0530

    Time: 57 ms (61.77%) | Memory: 32 MB (65.85%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -7,4 +7,21 @@ class Solution:
             max_sum = max(max_sum,current_sum)
             if current_sum < 0:
                 current_sum = 0
-        return max_sum        
+        return max_sum   
+        # start = end = temp = 0
+        #sub = []
+        # for ele in nums:
+        #     current_sum += ele
+        #     if current_sum > max_sum:
+        #         max_sum = current_sum
+        #         start = temp
+        #         end = i
+        #         sub = nums[start:end+1]
+        #     if current_sum < 0:
+        #         current_sum = 0
+        #         temp = i+1
+
+
+
+
+
