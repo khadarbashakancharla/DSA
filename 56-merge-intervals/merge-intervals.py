db25153fcdc42f6a5e6523ae6bc1ab287ef01999
khadarbashakancharla <122791410+khadarbashakancharla@users.noreply.github.com>
2025-03-11 19:13:20 +0530

    Time: 6 ms (78.32%) | Memory: 21.8 MB (14.60%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -3,15 +3,16 @@ class Solution:
         intervals.sort()
         start = intervals[0][0]
         end = intervals[0][1]
-        merged_intervals = []
-        for interval in intervals:
+        merging = []
+        for interval in intervals[1:]:
             if interval[0] <= end:
                 end = max(end,interval[1])
             else:
-                merged_intervals.append([start,end])
+                merging.append([start,end])
                 start = interval[0]
                 end = interval[1]
-        merged_intervals.append([start,end])
-        return merged_intervals            
+        merging.append([start,end])
+        return merging        
+
 
         
