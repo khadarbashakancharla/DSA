1ac2eedc33f2ed132ef36168b2a6b8ad67fc90ec
khadarbashakancharla <122791410+khadarbashakancharla@users.noreply.github.com>
2025-03-11 17:58:28 +0530

    Time: 8 ms (27.84%) | Memory: 17.7 MB (65.22%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,16 +1,13 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        sym = {
-            'I' : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, 'M' : 1000
-        }
-
-        t = 0
-        p = 0
-        for ele in reversed(s):
-            value = sym[ele]
-            if value < p:
-                t -= value
-            else:
-                t += value 
-                p = value
-        return t           
+        sym = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        total = 0
+        previous = 0
+        for digit in reversed(s):
+            if sym[digit] < previous:
+                total -= sym[digit]
+            else:    
+                total += sym[digit]
+                previous = sym[digit]
+        return total    
+                
