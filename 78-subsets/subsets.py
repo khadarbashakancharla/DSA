1f9372c5c48499fab79b38dcb1693bf22cbb90d6
khadarbashakancharla <122791410+khadarbashakancharla@users.noreply.github.com>
2025-03-11 21:32:42 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (64.82%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,7 +1,8 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         subsets = [[]]
-        for num in nums:
-            subsets += [current+[num] for current in subsets]
+        for ele in nums:
+            subsets += [current+[ele] for current in subsets]
         return subsets    
+
         
