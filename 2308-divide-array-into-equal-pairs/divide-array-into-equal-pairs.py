4f256d49d59b2926e9b381adcb3e093457a3fa58
khadarbashakancharla <122791410+khadarbashakancharla@users.noreply.github.com>
2025-03-17 16:00:39 +0530

    Time: 4 ms (42.68%) | Memory: 17.8 MB (73.03%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.py
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.py
@@ -1,9 +1,10 @@
+import collections 
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
-        unic_nums = set(nums)
-        for ele in unic_nums:
-            cnt = nums.count(ele)
-            if cnt%2 != 0:
+        count_of_digits = collections.Counter(nums)
+        for count in count_of_digits.values():
+            if count % 2 != 0:
                 return False
         return True        
+              
         
